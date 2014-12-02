325996fa3ea807cdd1a60733b8c5f1f430527e6a
Brytkov Dmitriy <dimoha@controlstyle.ru>
2014-12-03 02:16:05 +0300

    add get_semcore and parse_page_links methods

--- network/parser.py
+++ network/parser.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from pylibs.network import NetworkException
+from pylibs.network.urls import delete_http, get_domain, get_host, BadDomainNetworkException
 from lxml.cssselect import CSSSelector
 from lxml import etree, html
-import lxml, time, re
+import lxml, re
 from string import *
 
 
@@ -15,10 +16,12 @@ def print_element(element):
     lxml.html.etree.clear_error_log()
     print(pe)
 
+
 def css(tree, selectorSpec):
     sel = CSSSelector(selectorSpec)
     lxml.html.etree.clear_error_log()
     return sel(tree)
+
 
 def at_css(tree, selectorSpec):
     r = css(tree, selectorSpec)
@@ -28,6 +31,7 @@ def at_css(tree, selectorSpec):
     else:
         return None
 
+
 def at_xpath(e, path):
     r = e.xpath(path)
     lxml.html.etree.clear_error_log()
@@ -36,10 +40,12 @@ def at_xpath(e, path):
     else:
         return None
 
+
 def xpath(e, path):
     r = e.xpath(path)
     lxml.html.etree.clear_error_log()
     return r
+
 
 def element_text(e, method='text'):
     et = strip(etree.tostring(e, method=method, encoding="UTF-8"))
@@ -52,139 +58,115 @@ def parse_html(htmldata):
     lxml.html.etree.clear_error_log()
     return ph
 
+
 def parse_xml(xml):
     ph = etree.fromstring(xml)
     etree.clear_error_log()
     return ph
 
 
-def getPageLinks(url, html=None, options = {'type':'ext', 'noindex':False, 'nofollow':False}):
+def get_page_links(url, html_string, link_type='ext', is_noindex=False, is_nofollow=False):
     
-    allLinks = []
+    all_links = []
 
-    if html==None:
-        br = Browser()
-        br.get(url)
-        html = br.unicode()
+    html_string = re.sub('<!--\s*([/]?)noindex\s*-->(?is)', '<\\1noindex>', html_string)
+    html_string = re.sub('<!--.*?-->(?isu)', '', html_string)
+    html_string = re.sub('<style[^>]*>.*?</style>(?isu)', '', html_string)
+    html_string = re.sub('<script[^>]*>.*?</script>(?isu)', '', html_string)
     
-    html = re.sub('<!--\s*([/]?)noindex\s*-->(?is)', '<\\1noindex>', html)
-    html = re.sub('<!--.*?-->(?isu)', '', html)
-    html = re.sub('<style[^>]*>.*?</style>(?isu)', '', html)
-    html = re.sub('<script[^>]*>.*?</script>(?isu)', '', html)
-    
-
-    stop_domains = ['depositfiles.com', 'facebook.com', 'feedburner.com', 'google.com', 'letitbit.net', 'liveinternet.ru', 'mail.ru', 'okis.ru', 'rambler.ru', 'twitter.com', 'ucoz.ru', 'w3.org', 'vk.com', 'vkontakte.ru', 'wordpress.org', 'yandex.ru', 'youtube.com']
-
-    if html!='':
+    stop_domains = ['depositfiles.com', 'facebook.com', 'feedburner.com', 'google.com', 'letitbit.net',
+                    'liveinternet.ru', 'mail.ru', 'okis.ru', 'rambler.ru', 'twitter.com', 'ucoz.ru', 'w3.org',
+                    'vk.com', 'vkontakte.ru', 'wordpress.org', 'yandex.ru', 'youtube.com']
+    if html_string != '':
         try:
-            html = parse_html(html)
+            html_string = parse_html(html_string)
         except Exception as e:
-            return allLinks
+            return all_links
     else:
-        return allLinks
+        return all_links
     
     # create manual base href
-    baseHref = delete_http(url).split('/')
-    if len(baseHref)>1:
-        baseHref.pop()
-    baseHref = 'http://'+"/".join(baseHref)+"/"
+    base_href = delete_http(url).split('/')
+    if len(base_href) > 1:
+        base_href.pop()
+    base_href = 'http://'+"/".join(base_href)+"/"
 
-    
     # try parse base href from html
-    baseHref_tmp = at_xpath(html, './/head/base/@href')
-    if baseHref_tmp!=None:
-        tmp = str(baseHref_tmp)
+    base_href_tmp = at_xpath(html_string, './/head/base/@href')
+    if base_href_tmp is not None:
+        tmp = str(base_href_tmp)
         try:
-            if domain(tmp)==domain(url):
-                baseHref = tmp.strip()
+            if get_domain(tmp) == get_domain(url):
+                base_href = tmp.strip()
         except:
             pass
     
     # this domain
-    tDomain = domain(baseHref)
-    tHost = get_host(baseHref)
+    this_domain = get_domain(base_href)
+    this_host = get_host(base_href)
     
-    rFilter = re.compile(u"[^/]+/\.\.(?is)")
+    r_filter = re.compile(u"[^/]+/\.\.(?is)")
     
-    if 'nofollow' in options and not options['nofollow']:
-        no_get_nofollow = True
-    else:
-        no_get_nofollow = False
-  
-    links = xpath(html, './/a[@href]')
+    links = xpath(html_string, './/a[@href]')
     for link in links:
-        
         href = at_xpath(link, '@href')
         
         if 'javascript' in href.lower() or 'mailto' in href.lower():
             continue
         
         anchor = element_text(link).strip()
-        if anchor=='':
+        if anchor == '':
             anchor = None
 
-    
         # detect noindex
         noindex = at_xpath(link, 'ancestor::noindex')
         noindex = not noindex is None
 
-
-        if 'noindex' in options and options['noindex']==False and noindex:
+        if not is_noindex and noindex:
             continue
-        
-        
-        
+
         # detect nofollow
         rel = at_xpath(link, '@rel')
-        if rel!=None and ' nofollow ' in ' '+str(rel)+' ':
-            nofollow = True
-        else:
-            nofollow = False
-    
+        nofollow = True if rel is not None and ' nofollow ' in ' %s ' % rel else False
         m = re.search('(http[s]?):\/\/(?is)', href)
         if m:
-            protocol = m.group(1).strip()
-            tHost = get_host(href)
-            href = href.replace(tHost, tHost.lower())
-            href = href.replace(protocol, protocol.lower())
+            this_protocol = m.group(1).strip()
+            this_host_local = get_host(href)
+            href = href.replace(this_host_local, this_host_local.lower())
+            href = href.replace(this_protocol, this_protocol.lower())
 
         href = href.strip(" \"'\n\r")
 
-        if href=='':
+        if href == '':
             href = '/'
 
         m1 = re.search('^http[s]?://(?is)', href)
-        m2 = re.search('^http[s]?://([^/\?:]+\.|)'+tDomain+'(?is)', href) 
+        m2 = re.search('^http[s]?://([^/\?:]+\.|)'+this_domain+'(?is)', href)
 
-    
-        if m1==None or m2:
-            lType = 'int'
-        else:
-            lType = 'ext'
-            
-        if 'type' in options and options['type']!=lType:
+        l_type = 'int' if m1 is None or m2 else 'ext'
+
+        if link_type != l_type:
             continue
 
-                    
-        if lType=='int':
-            if m1==None:
-                if href[0:2]=='./':
+        if l_type == 'int':
+            if m1 is None:
+                if href[0:2] == './':
                     href = href[2:].strip()
-                    if href=='':
+                    if href == '':
                         href = '/'
 
-                if href[0]!='/': 
-                    href = baseHref+href
+                if href[0] != '/':
+                    href = base_href+href
                 else:
-                    href = 'http://'+tHost+'/'+href
+                    href = 'http://%s/%s' % (this_host, href)
 
                 # replace double "/"
                 href = re.sub('/+(?is)', '/', delete_http(href))                        
 
-                while re.search(rFilter, href):
-                    href = re.sub(rFilter, '/', href)    
+                while re.search(r_filter, href):
+                    href = re.sub(r_filter, '/', href)
 
-                href = 'http://'+href;
+                href = 'http://%s' % href
                 href = href.replace('../', '')
             href = re.sub('#.*$(?is)', '', href)    
         else:
@@ -198,14 +180,13 @@ def getPageLinks(url, html=None, options = {'type':'ext', 'noindex':False, 'nofo
             if lnk_domain in stop_domains or lnk_domain_2 in stop_domains:
                 nofollow = True
             
-        if no_get_nofollow and nofollow:
+        if not is_nofollow and nofollow:
             continue
         
-        allLinks.append({'href':str(href), 'anchor':str(anchor), 'noindex':noindex, 'nofollow':nofollow, 'type':lType})
+        all_links.append({'href': str(href), 'anchor': str(anchor), 'noindex': noindex,
+                          'nofollow': nofollow, 'type': l_type})
 
-    return allLinks
+    return all_links
 
 
-def strip_tags(value):
-    return re.sub('<[^>]*?>(?is)', '', value)
 
--- utils/tools.py
+++ utils/tools.py
@@ -6,9 +6,9 @@ def kill_process(mask):
     c = 0
     tPid = str(os.getpid())
     command = "ps aux | grep '"+str(mask)+"' | grep -v 'defunct' | grep -v grep | grep -v "+str(tPid)+""
-    info("kill_process command: %s" % command)
+    debug("kill_process command: %s" % command)
     processes = os.popen(command).read().strip()
-    info("kill_process processes: %s" % processes)
+    debug("kill_process processes: %s" % processes)
     for onestr in processes.split("\n"):
         try:
             thisStrPid = onestr.split()[1]
@@ -31,9 +31,9 @@ def processecControl(name, numcopies, args = None, no_exit = False):
         eArgs = []
         canCollect = False
         command = "ps aux | grep '"+str(name)+"' | grep -v grep | grep -v 'bin/sh' | grep -v "+str(tPid)
-        info("processecControl command: %s" % command)
+        debug("processecControl command: %s" % command)
         processes = os.popen(command).read().strip()
-        info("processecControl processes: %s" % processes)
+        debug("processecControl processes: %s" % processes)
         alist = processes.split(" ")
         for v in alist:
             v = v.strip()

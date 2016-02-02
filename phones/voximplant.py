778b5c2ba74198e0518731c51e8af991cb35e43c
Brytkov Dmitriy <d.brytkov@dev.kokoc.com>
2016-02-02 13:30:13 +0300

    upgrade voximplant

--- phones/voximplant.py
+++ phones/voximplant.py
@@ -25,14 +25,15 @@ class VoxImplantAPI(object):
         self.account_id = account_id
         self.api_key = api_key
 
-    def __request(self, method, data):
-        request_url = '{0}{1}/?account_id={2}&api_key={3}&{4}'.format(
-            self.api_url,
-            method,
-            self.account_id,
-            self.api_key,
-            urllib.urlencode(data)
-        )
+    def __request(self, method=None, data=None, request_url=None):
+        if request_url is None:
+            request_url = '{0}{1}/?account_id={2}&api_key={3}&{4}'.format(
+                self.api_url,
+                method,
+                self.account_id,
+                self.api_key,
+                urllib.urlencode(data)
+            )
         logging.info("Request to {0}".format(request_url))
         r = requests.get(request_url)
         logging.info("response: {0}".format(r.text))
@@ -45,8 +46,8 @@ class VoxImplantAPI(object):
         except ValueError:
             raise VoxImplantApiException("bad response: {0}".format(r.text))
 
-        if response['result'] != 1:
-            raise VoxImplantApiException(response)
+        if 'error' in response:
+            raise VoxImplantApiException(response['error']['msg'])
 
         return response
 
@@ -56,4 +57,22 @@ class VoxImplantAPI(object):
             'script_custom_data': ":".join([phone_from, phone_to]),
         }
         response = self.__request('StartScenarios', data)
+
+        if response['result'] != 1:
+            raise VoxImplantApiException(response)
+
         return response
+
+    def request_media_session_access_url(self, url):
+        return self.__request(request_url=url)
+
+    def get_history(self, date_from, date_to, custom_data):
+        frmt = "%Y-%m-%d %H:%M:%s"
+        data = {
+            'from_date': date_from.strftime(frmt),
+            'to_date': date_to.strftime(frmt),
+            'with_calls': 1,
+            'call_session_history_custom_data': custom_data
+        }
+        response = self.__request('GetCallHistory', data)
+        return response['result']

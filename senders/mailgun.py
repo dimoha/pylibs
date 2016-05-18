88deafff991761fc96e677004e6861d93298a636
Brytkov Dmitriy <d.brytkov@dev.kokoc.com>
2016-05-18 20:21:41 +0300

    upgrade mailgun api

--- senders/mailgun.py
+++ senders/mailgun.py
@@ -4,6 +4,8 @@ import requests
 import json
 import logging
 import re
+import time
+from decimal import Decimal
 
 
 class MailGunException(SendersException):
@@ -29,18 +31,62 @@ class MailGunApi(object):
         self.api_key = api_key
         self.domain = domain
 
-    def __request(self, method, post_params):
+    def __request(self, api_method=None, params=None, method='POST', url=None):
 
-        r = requests.post("{0}{1}/{2}".format(self.api_url, self.domain, method), auth=("api", self.api_key), data=post_params)
-        if r.status_code != 200:
-            raise MailGunApiBadHttpException(r.status_code)
+        if url is None:
+            request_url = "{0}{1}/{2}".format(self.api_url, self.domain, api_method)
+        else:
+            request_url = url
+            method = 'GET'
+            params = None
+
+        logging.debug(request_url)
+
+        if method == 'POST':
+            r = requests.post(request_url, auth=("api", self.api_key), data=params)
+        else:
+            r = requests.get(request_url,  auth=("api", self.api_key), params=params)
 
         try:
             response = json.loads(r.text)
         except ValueError:
+            response = None
+
+        if r.status_code != 200:
+            err_msg = response['message'] if response and 'message' in response else r.status_code
+            raise MailGunApiBadHttpException(err_msg)
+
+        if response is None:
             raise MailGunApiException("bad response: {0}".format(r.text))
 
         return response
+
+    def get_events(self, event_types, from_ut=None):
+
+        year_ago = Decimal(str(time.time() - 86400*365))
+        if from_ut is not None:
+            from_ut = Decimal(str(from_ut))
+
+        if from_ut is None or from_ut < year_ago:
+            from_ut = year_ago
+
+        params = {
+            "event": " OR ".join(event_types),
+            "begin": from_ut,
+            "end": time.time(),
+            "limit": 300
+        }
+
+        res = self.__request('events', params, method='GET')
+        events = res['items']
+
+        while 'paging' in res and 'next' in res['paging']:
+            res = self.__request(url=res['paging']['next'])
+            events += res['items']
+            if len(res['items']) == 0:
+                break
+
+        return events
 
     def send_mail(self, subject, body, recipients, sender):
 

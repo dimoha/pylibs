673cbbb306576e55ca27d6997358fb0e964376d1
Brytkov Dmitriy <d.brytkov@dev.kokoc.com>
2017-11-13 18:41:17 +0300

    add gdeslon postback and revise

--- pylibs/cpa/gdeslon.py
+++ pylibs/cpa/gdeslon.py
@@ -3,6 +3,7 @@ from pylibs.cpa import CpaException
 import requests
 import json
 import logging
+import xml.etree.ElementTree as ET
 
 
 class GdeSlonException(CpaException):
@@ -14,6 +15,14 @@ class GdeSlonApiException(GdeSlonException):
 
 
 class GdeSlonApiBadHttpException(GdeSlonApiException):
+    pass
+
+
+class OrderNotExist(GdeSlonApiException):
+    pass
+
+
+class OrderAlreadyPayed(GdeSlonApiException):
     pass
 
 
@@ -58,6 +67,35 @@ class GdeSlonApi(object):
 
         return response
 
+    def __request_xml(self, api_method, params=None):
+        request_url = "{0}{1}".format(self.api_url, api_method)
+        if params is None:
+            params = {}
+
+        params['_gs_at'] = self.api_key
+
+        r = requests.get(
+            request_url,
+            data=params,
+        )
+
+        try:
+            response = ET.fromstring(r.text)
+        except ET.ParseError:
+            response = None
+            logging.error(r.text)
+
+        if r.status_code != 200:
+            raise GdeSlonApiBadHttpException(r.status_code)
+
+        if response is None:
+            raise GdeSlonApiException(u"bad response: {0}".format(r.text))
+
+        if len(response) > 1 and response[1].text == u'failure':
+            raise GdeSlonApiException(response[0].text)
+
+        return response
+
     def get_orders(self, from_dt, period):
 
         params = {
@@ -68,3 +106,32 @@ class GdeSlonApi(object):
         }
 
         return self.__request('orders', params)
+
+    def set_order_status(self, order_id, status):
+
+        try:
+            xml_res = self.__request_xml('states.xml', {
+                'order_ids[]': order_id,
+                'state': status
+            })
+            if xml_res[0].text != 'success':
+                raise GdeSlonApiException(u"set_order_status bad response: {0}".format(xml_res[0].text))
+        except GdeSlonApiException as e:
+            if u'We found no orders' in str(e):
+                raise OrderNotExist(order_id)
+            elif u'Unable to change' in str(e):
+                raise OrderAlreadyPayed(order_id)
+            else:
+                raise
+
+    def postback(self, params):
+        payload = {
+            'codes': params['codes'],
+            'order_id': params['id'],
+            'merchant_id': params['merchant_id'],
+            'token': params['lead_id']
+        }
+        logging.info(payload)
+        r = requests.get('https://www.gdeslon.ru/purchase.js', params=payload)
+        logging.info(r.status_code)
+        return r.status_code == requests.codes.ok
